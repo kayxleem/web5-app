3bf871dc2ff201ffc60c3831973db3229e334709
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-11-28 00:02:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,23 +13,24 @@ if (!globalThis.crypto) globalThis.crypto = webcrypto;
 //connect
  const { web5, did: aliceDid } = await Web5.connect();
 
-//console.log(aliceDid);
+console.log(aliceDid);
 
 //create
-// const { record } = await web5.dwn.records.create({
-//     data: 'Hello, Web5!, This is kayode',
-//     message: {
-//       dataFormat: 'text/plain',
-//     },
-//   });
+const { record } = await web5.dwn.records.create({
+    data: 'Hello, Web5!, This is kayode',
+    message: {
+      dataFormat: 'text/plain',
+    },
+  });
 
-//console.log('writeResult', record);
+console.log('writeResult', record);
 
 //read
 const readResult = await record.data.text();
 
 console.log('readResult', readResult)
 
+//update
 const updateResult = await record.update({
     data: 'Hello, Web5! I am updated.',
   });
@@ -37,6 +38,6 @@ const updateResult = await record.update({
 console.log('updateResult', await record.data.text())
 
 //delete
-//const deleteResult = await record.delete();
+const deleteResult = await record.delete();
 
-//console.log('deleteResult', deleteResult)
+console.log('deleteResult', deleteResult)
